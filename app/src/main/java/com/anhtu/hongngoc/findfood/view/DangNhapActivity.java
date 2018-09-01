89c49c4c8d22ce7c75fc9c6ea4317dd5716f5d8e
tuna <nguyenanhtu@hou.edu.vn>
2018-09-01 17:37:27 +0700

    giao diện đăng nhập part2

--- app/src/main/java/com/anhtu/hongngoc/findfood/view/DangNhapActivity.java
+++ app/src/main/java/com/anhtu/hongngoc/findfood/view/DangNhapActivity.java
@@ -5,12 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.anhtu.hongngoc.findfood.R;
+import com.facebook.FacebookSdk;
 
 public class DangNhapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(FacebookSdk.getApplicationContext());
         setContentView(R.layout.layout_dangnhap);
     }
 }
