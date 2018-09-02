e6427f0fc29d934fa7b99b44d5a96504f9f8565f
tuna <nguyenanhtu@hou.edu.vn>
2018-09-02 14:06:40 +0700

    Đăng nhập google

--- app/src/main/java/com/anhtu/hongngoc/findfood/view/DangNhapActivity.java
+++ app/src/main/java/com/anhtu/hongngoc/findfood/view/DangNhapActivity.java
@@ -1,18 +1,142 @@
 package com.anhtu.hongngoc.findfood.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.anhtu.hongngoc.findfood.R;
 import com.facebook.FacebookSdk;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class DangNhapActivity extends AppCompatActivity {
+public class DangNhapActivity extends AppCompatActivity implements View.OnClickListener, FirebaseAuth.AuthStateListener{
+
+    private Button btnDangNhapGoogle;
+
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
+    public static int KIEMTRA_PROVIDER_DANGNHAP = 0;
+
+    // [START declare_auth]
+    private FirebaseAuth firebaseAuth;
+    // [END declare_auth]
+
+    private GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(FacebookSdk.getApplicationContext());
         setContentView(R.layout.layout_dangnhap);
+
+        // [START initialize_auth]
+        firebaseAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        btnDangNhapGoogle = (Button) findViewById(R.id.btnDangNhapGoogle);
+        btnDangNhapGoogle.setOnClickListener(this);
+
+        taoClientDangNhapGoogle();
     }
+
+    private void taoClientDangNhapGoogle() {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, signInOptions);
+    }
+
+    private void dangNhapGoogle() {
+        // mở form đăng nhập google
+        KIEMTRA_PROVIDER_DANGNHAP = 1;
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    chungThucDangNhapFireBase(account);
+                } catch (ApiException e) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w(TAG, "Google sign in failed", e);
+                    // ...
+                };
+            }
+        }
+    }
+
+    private void chungThucDangNhapFireBase(GoogleSignInAccount account) {
+        if (KIEMTRA_PROVIDER_DANGNHAP == 1) {
+            // Lấy stokenID đã đăng nhập bằng google để đăng nhập trên Firebase
+            Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+            firebaseAuth.signInWithCredential(credential);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btnDangNhapGoogle:
+                dangNhapGoogle();
+                break;
+        }
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        // kiểm tra người dùng đăng nhập thành công hay đăng suất
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            Intent iTrangChu = new Intent(this, TrangChuActivity.class);
+            startActivity(iTrangChu);
+        } else {
+
+        }
+    }
+
 }
