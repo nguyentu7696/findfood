274c9e164f211c8ccbda518e9c7628ed14032eb6
tuna <nguyenanhtu@hou.edu.vn>
2018-10-23 13:17:12 +0700

    Hiển thị thông tin wifi của quán ắn

--- app/src/main/java/com/anhtu/hongngoc/findfood/view/ChiTietQuanAnActivity.java
+++ app/src/main/java/com/anhtu/hongngoc/findfood/view/ChiTietQuanAnActivity.java
@@ -1,10 +1,14 @@
 package com.anhtu.hongngoc.findfood.view;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,19 +24,35 @@ import android.widget.VideoView;
 
 import com.anhtu.hongngoc.findfood.Adapters.ApdaterBinhLuan;
 import com.anhtu.hongngoc.findfood.R;
+import com.anhtu.hongngoc.findfood.controller.ChiTietQuanController;
 import com.anhtu.hongngoc.findfood.model.QuanAnModel;
+import com.anhtu.hongngoc.findfood.model.TienIchModel;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class ChiTietQuanAnActivity extends AppCompatActivity {
-    private TextView txtTenQuanAn,txtDiaChi,txtThoiGianHoatDong,txtTrangThaiHoatDong,txtTongSoHinhAnh,txtTongSoBinhLuan,txtTongSoCheckIn,txtTongSoLuuLai,txtTieuDeToolbar,txtGioiHanGia,txtTenWifi,txtMatKhauWifi,txtNgayDangWifi;
-    private ImageView imHinhAnhQuanAn,imgPlayTrailer;
+public class ChiTietQuanAnActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
+    private TextView txtTenQuanAn, txtDiaChi, txtThoiGianHoatDong, txtTrangThaiHoatDong, txtTongSoHinhAnh, txtTongSoBinhLuan, txtTongSoCheckIn, txtTongSoLuuLai, txtTieuDeToolbar, txtGioiHanGia, txtTenWifi, txtMatKhauWifi, txtNgayDangWifi;
+    private ImageView imHinhAnhQuanAn, imgPlayTrailer;
     private Button btnBinhLuan;
     private LinearLayout khungWifi;
     private QuanAnModel quanAnModel;
@@ -43,6 +63,10 @@ public class ChiTietQuanAnActivity extends AppCompatActivity {
     private VideoView videoView;
     private RecyclerView recyclerThucDon;
     private ApdaterBinhLuan adapterBinhLuan;
+    private GoogleMap googleMap;
+    private MapFragment mapFragment;
+
+    private ChiTietQuanController chiTietQuanController;
 
 
     @Override
@@ -67,13 +91,24 @@ public class ChiTietQuanAnActivity extends AppCompatActivity {
         btnBinhLuan = (Button) findViewById(R.id.btnBinhLuan);
         videoView = (VideoView) findViewById(R.id.videoTrailer);
         recyclerViewBinhLuan = (RecyclerView) findViewById(R.id.recyclerBinhLuanChiTietQuanAn);
-
-        hienThiChiTietQuanAn();
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        txtGioiHanGia = (TextView) findViewById(R.id.txtGioiHanGia);
+        khungTienIch = (LinearLayout) findViewById(R.id.khungTienTich);
+        txtTenWifi = (TextView) findViewById(R.id.txtTenWifi);
+        txtMatKhauWifi = (TextView) findViewById(R.id.txtMatKhauWifi);
+        khungWifi = (LinearLayout) findViewById(R.id.khungWifi);
+        txtNgayDangWifi = (TextView) findViewById(R.id.txtNgayDangWifi);
 
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        chiTietQuanController = new ChiTietQuanController();
+
+        mapFragment.getMapAsync(ChiTietQuanAnActivity.this);
+
+        hienThiChiTietQuanAn();
     }
 
     @Override
@@ -82,7 +117,7 @@ public class ChiTietQuanAnActivity extends AppCompatActivity {
         return true;
     }
 
-    private void hienThiChiTietQuanAn(){
+    private void hienThiChiTietQuanAn() {
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
 
@@ -95,10 +130,10 @@ public class ChiTietQuanAnActivity extends AppCompatActivity {
             Date dateMoCua = dateFormat.parse(giomocua);
             Date dateDongCua = dateFormat.parse(giodongcua);
 
-            if(dateHienTai.after(dateMoCua) && dateHienTai.before(dateDongCua)){
+            if (dateHienTai.after(dateMoCua) && dateHienTai.before(dateDongCua)) {
                 //gio mo cua
                 txtTrangThaiHoatDong.setText(getString(R.string.dangmocua));
-            }else{
+            } else {
                 //dong cua
                 txtTrangThaiHoatDong.setText(getString(R.string.dadongcua));
             }
@@ -116,12 +151,23 @@ public class ChiTietQuanAnActivity extends AppCompatActivity {
         txtTongSoBinhLuan.setText(quanAnModel.getBinhLuanModelList().size() + "");
         txtThoiGianHoatDong.setText(giomocua + " - " + giodongcua);
 
+        downLoadHinhTienIch();
+
+        if (quanAnModel.getGiatoida() != 0 && quanAnModel.getGiatoithieu() != 0) {
+            NumberFormat numberFormat = new DecimalFormat("###,###");
+            String giatoithieu = numberFormat.format(quanAnModel.getGiatoithieu()) + " đ";
+            String giatoida = numberFormat.format(quanAnModel.getGiatoida()) + " đ";
+            txtGioiHanGia.setText(giatoithieu + " - " + giatoida);
+        } else {
+            txtGioiHanGia.setVisibility(View.INVISIBLE);
+        }
+
         StorageReference storageHinhQuanAn = FirebaseStorage.getInstance().getReference().child("hinhquanan").child(quanAnModel.getHinhanhquanan().get(0));
         long ONE_MEGABYTE = 1024 * 1024;
         storageHinhQuanAn.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
             @Override
             public void onSuccess(byte[] bytes) {
-                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                 imHinhAnhQuanAn.setImageBitmap(bitmap);
             }
         });
@@ -129,13 +175,100 @@ public class ChiTietQuanAnActivity extends AppCompatActivity {
         //Load danh sach binh luan cua quan
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerViewBinhLuan.setLayoutManager(layoutManager);
-        adapterBinhLuan = new ApdaterBinhLuan(this,R.layout.custom_layout_binhluan,quanAnModel.getBinhLuanModelList());
+        adapterBinhLuan = new ApdaterBinhLuan(this, R.layout.custom_layout_binhluan, quanAnModel.getBinhLuanModelList());
         recyclerViewBinhLuan.setAdapter(adapterBinhLuan);
         adapterBinhLuan.notifyDataSetChanged();
+
+        NestedScrollView nestedScrollViewChiTiet = (NestedScrollView) findViewById(R.id.nestScrollViewChiTiet);
+        nestedScrollViewChiTiet.smoothScrollTo(0,0);
+
+        chiTietQuanController.HienThiDanhSachWifiQuanAn(quanAnModel.getMaquanan(), txtTenWifi, txtMatKhauWifi, txtNgayDangWifi);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+//        this.googleMap = googleMap;
+//
+//        double latitude = quanAnModel.getChiNhanhQuanAnModelList().get(0).getLatitude();
+//        double longitude = quanAnModel.getChiNhanhQuanAnModelList().get(0).getLongitude();
+//
+//        LatLng latLng = new LatLng(latitude,longitude);
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(latLng);
+//        markerOptions.title(quanAnModel.getTenquanan());
+//
+//        googleMap.addMarker(markerOptions);
+//
+//        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,14);
+//        googleMap.moveCamera(cameraUpdate);
+        this.googleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        LatLng XXX = new LatLng(21.014557, 105.804000);
+        googleMap.addMarker(new MarkerOptions().position(XXX).title("XXX"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(XXX, 18));
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+    }
+
+    private void downLoadHinhTienIch(){
+
+        for (String matienich : quanAnModel.getTienich()){
+            DatabaseReference nodeTienIch = FirebaseDatabase.getInstance().getReference().child("quanlytienichs").child(matienich);
+            nodeTienIch.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    TienIchModel tienIchModel = dataSnapshot.getValue(TienIchModel.class);
+
+                    StorageReference storageHinhQuanAn = FirebaseStorage.getInstance().getReference().child("hinhtienich").child(tienIchModel.getHinhtienich());
+                    long ONE_MEGABYTE = 1024 * 1024;
+                    storageHinhQuanAn.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                            ImageView imageTienIch = new ImageView(ChiTietQuanAnActivity.this);
+                            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100,100);
+                            layoutParams.setMargins(10,10,10,10);
+                            imageTienIch.setLayoutParams(layoutParams);
+                            imageTienIch.setScaleType(ImageView.ScaleType.FIT_XY);
+                            imageTienIch.setPadding(5,5,5,5);
+
+
+                            imageTienIch.setImageBitmap(bitmap);
+                            khungTienIch.addView(imageTienIch);
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+
     }
 }
